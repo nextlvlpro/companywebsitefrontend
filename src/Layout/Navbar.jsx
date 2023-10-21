62bd48f84cc003eafa41baab5b3c834f8706ec07
nextlvlpro <80281253+nextlvlpro@users.noreply.github.com>
2023-10-21 16:54:39 +0530

    Update Navbar.jsx

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -78,7 +78,10 @@ export default function Navbar() {
 
                   <div className='flex items-center justify-center gap-3'>
                     <div className=' cursor-none'>
-                      {user.userName}
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+</svg>
+
                     </div>
                     <div className=' cursor-pointer'>
                       <Link to={'/logout'}>
