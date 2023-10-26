1c4d78fbdfbb13eac24c3145f0ba66866061bbf6
nextlvlpro <80281253+nextlvlpro@users.noreply.github.com>
2023-10-26 13:11:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import axios from 'axios'
 import UserContextProvider from './UserContext'
 import Logout from './Pages/LoginandRegister/Logout'
 
-axios.defaults.baseURL = 'https://medplbackend.onrender.com';
+axios.defaults.baseURL = 'https://medpl.onrender.com';
 axios.defaults.withCredentials = true
 
 function App() {
