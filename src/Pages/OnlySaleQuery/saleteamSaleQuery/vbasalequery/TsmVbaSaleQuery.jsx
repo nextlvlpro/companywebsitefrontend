576887382096aa7dbcad0f6dcb177c1defaa2bb1
nextlvlpro <80281253+nextlvlpro@users.noreply.github.com>
2023-11-07 12:45:50 +0530

    Add files via upload

--- src/Pages/OnlySaleQuery/saleteamSaleQuery/vbasalequery/TsmVbaSaleQuery.jsx
+++ src/Pages/OnlySaleQuery/saleteamSaleQuery/vbasalequery/TsmVbaSaleQuery.jsx
@@ -4,57 +4,71 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
 export default function TsmVbaSaleQuery() {
-  const {user} = useContext(UserContext)
+  const { user } = useContext(UserContext)
   const [vbasaledata, setVbasaledata] = useState(null)
   const [loading, setLoading] = useState(false)
   const tsmArea = useParams()
-  useEffect(()=> {
+  useEffect(() => {
     setLoading(true)
-    if(user) {
-      axios.post('/tsmareavbasalequery', tsmArea).then(({data}) => {
+    if (user) {
+      axios.post('/tsmareavbasalequery', tsmArea).then(({ data }) => {
         setLoading(false)
         setVbasaledata(data)
       })
     }
-  },[user])
+  }, [user])
 
   return (
     <div>
-      <div className='w-full flex flex-col py-5 m-2'>
-      <div className='bg-blue-500 p-2 text-white rounded text-center w-[200px] m-auto mb-2'>
-      VBA Sale Query
+      <div className='flex flex-col py-5 m-2'>
+        <div className='bg-blue-500 p-2 text-white rounded text-center w-[200px] m-auto mb-2'>
+          VBA Sale Query {`(${tsmArea.tsmarea})`}
+        </div>
+
+        {vbasaledata?.map((items, i) => (
+          <div key={i} className='flex flex-col text-left shadow-sm shadow-blue-500  border border-blue-500 rounded-xl items-center justify-center p-1 mb-2 bg-gray-300'>
+            <div>
+              <div className='bg-blue-500 text-white p-1 rounded'>
+                {items?.vbaname}
+              </div>
+            </div>
+
+            <div>
+              <div className='flex items-center justify-center gap-2 px-2 py-1'>
+                <div className='border-r border-gray-500 px-2 text-blue-500'>
+                  <b>Shop: {items?.vworkshopname.split("(")[0]}</b>
+                </div>
+                <div className='border-r border-gray-500 px-2 py-1'>
+                  Stock: {items.currentstock}
+                </div>
+              </div>
+              <div className='flex items-center justify-center gap-2 '>
+                <div className='border-r border-gray-500 px-2 py-1'>
+                  Target: {items.target}
+                </div>
+                <div className='border-r border-gray-500 px-2 py-1'>
+                  Ach: {items.ach}
+                </div>
+                <div>
+                  Ach %: {((items.ach / items.target) * 100).toFixed(1)}%
+                </div>
+              </div>
+              <div className='flex items-center justify-center gap-2 '>
+                <div className='border-r border-gray-500 px-2 py-1'>
+                  Last Month Sale: {items.lastmonthsale}
+                </div>
+              </div>
+            </div>
+
+          </div>
+        ))}
+
+        {!!loading && (
+          <div className='m-auto w-full text-center'>
+            Loading....
+          </div>
+        )}
       </div>
-      {vbasaledata?.map((items,i) => (
-        <div key={i} className='flex md:flex-row flex-col gap-2 shadow-md shadow-gray-400 mb-3 rounded-2xl md:items-center md:justify-evenly bg-gray-300 text-black font-medium p-2'>
-            <div className='md:border-r border-gray-400 pl-3 p-1 md:w-[200px] md:bg-none bg-blue-500 rounded-lg text-white'>
-                VBA Name : {items.vbaname}
-            </div>
-            <div className='md:border-r border-gray-400 p-1 w-[300px]'>
-                Shop Name : {items.vworkshopname}
-            </div>
-            <div className='md:border-r border-gray-400 p-1 w-[100px]'>
-                Stock : {items.currentstock}
-            </div>
-            <div className='md:border-r border-gray-400 p-1 w-[100px]'>
-            Target : {items.target}
-            </div>
-            <div className='md:border-r border-gray-400 p-1 w-[150px]'>
-              Ach : {items.ach}
-            </div>
-            <div className='md:border-r border-gray-400 p-1 w-[150px]  '>
-              Ach% : {((items.ach/items.target)*100).toFixed(1)} %
-            </div>
-            <div className='p-1 w-[200px]'>
-              Last Month Ach : {items.lastmonthsale}
-            </div>
-        </div>
-      ))}
-      {!!loading && (
-        <div className='m-auto w-full text-center'>
-          Loading....
-        </div>
-      )}
-    </div>
     </div>
   )
 }
