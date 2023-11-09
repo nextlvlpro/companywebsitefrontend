f793d0a3d3052cee83835b392a6fd9582166fb20
nextlvlpro <80281253+nextlvlpro@users.noreply.github.com>
2023-11-09 13:15:41 +0530

    Delete src directory

--- src/Pages/Contact/RetailerHelpLine.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function RetailerHelpLine() {
-  return (
-    <div>
-      Retailer Helpline is currently down
-    </div>
-  )
-}
--- src/Pages/Contact/VbaContact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function VbaContact() {
-  return (
-    <div>
-      VBA HelpLine is currently down
-    </div>
-  )
-}
